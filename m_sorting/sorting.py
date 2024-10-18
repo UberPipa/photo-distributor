2c10855c07e95fb980f9c31d7543e2a4583cd84a
tema <nu.takoe.bro@yandex.ru>
2024-10-18 09:33:40 +0300

    0.0.3 - добавил сортировку по метаданным

---
+++ m_sorting/sorting.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+
+from base.basicProcess.basicCheck import check_type_file
+from base.basicProcess.basicData import get_full_basic_data
+from base.metaProcess.main import getEasyMeta
+import time
+
+
+def sorting_for_meta(source_dir) -> None:
+    """
+    Сортирует файлы по месяцам и годам. если в них есть метаданные. Если нет, то пропускает,
+    сохраняет структуру папок.
+    :param source_dir:
+    :return:
+    """
+    s = 0
+    for location, dirs, files in os.walk(source_dir):
+        for file in os.listdir(location):
+            if check_type_file(file):
+                basic_data = get_full_basic_data(file, location)
+                full_name = basic_data['full_name']
+                name = basic_data['name']
+                extension = basic_data['extension']
+                type_file = basic_data['type_file']
+                location_file = basic_data['location_file']
+
+
+                meta = getEasyMeta(basic_data)
+
+
+                if meta:
+                    s += 1
+                    year = meta[:4]
+                    month = meta[5:7]
+
+                    location_year = os.path.join(location, str(year))
+                    fixed_length = 2
+                    location_month_in_year = os.path.join(location_year, f"{str(month).zfill(fixed_length)}")
+
+                    if not os.path.exists(location_year):
+                        """ Проверяем, есть ли dir года для этого файла, если нет, то создаём """
+                        os.makedirs(os.path.join(location_year))
+                        time.sleep(0.01)
+
+                    if not os.path.exists(location_month_in_year):
+                        """ Проверяем, есть ли dir месяца для этого файла, если нет, то создаём """
+                        os.makedirs(os.path.join(location_month_in_year))
+                        time.sleep(0.01)
+
+                    """ Двигаем файлы """
+                    shutil.move(location_file, location_month_in_year)
+                    time.sleep(0.01)
+
+    print(f"Всего отсортировано файлов по метаданным: {s}.")
+
+
+def sorting_for_name(source_dir) -> None:
+    """
+    Сортирует файлы по месяцам и годам. если удалось спарсить дату из имени. Если нет, то пропускает,
+    сохраняет структуру папок.
+    :param source_dir:
+    :return:
+    """
+
+    print('Пока не готово')
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from base.example.example import get_info_all_files
 import argparse
 
 from m_report.report import report
+from m_sorting.sorting import sorting_for_meta, sorting_for_name
 
 
 def run() -> None:
@@ -21,15 +22,11 @@ def run() -> None:
     parser.add_argument('-d', '--directory', type=str, required=True,  help="Директория для обработки на вход")
     parser.add_argument('-i', '--info', action='store_true', help="В указанной директории пробегает по каждому файлу и выводит по нему информацию.")
     parser.add_argument('-r', '--report', action='store_true', help="По указанной директории выводит побробную информацию по всем файлам.")
-    #parser.add_argument('-sM', '--sorting-meta', action='store_true', help="В указанной директории выполняет сортировку файлов по месяцам и годам исходя из метаданных.")
-    #parser.add_argument('-sN', '--sorting-name', action='store_true', help="В указанной директории выполняет сортировку файлов по месяцам и годам исходя из имени файлов.")
-
-
-
+    parser.add_argument('-sM', '--sorting-meta', action='store_true', help="В указанной директории выполняет сортировку файлов по месяцам и годам исходя из метаданных.")
+    parser.add_argument('-sN', '--sorting-name', action='store_true', help="В указанной директории выполняет сортировку файлов по месяцам и годам исходя из имени файлов.")
 
     # Парсим
     args = parser.parse_args()
-
     # Содержит директорию источник
     source_dir = args.directory
 
@@ -37,13 +34,23 @@ def run() -> None:
     if not os.path.isdir(source_dir):
         print(f"Ошибка: '{source_dir}' не существует или это не директория, укажите директорию.")
         sys.exit(1)
+    else:
+        # Проверяем флаги
+        if args.info:
+            get_info_all_files(source_dir)
 
+        if args.report:
+            report(source_dir)
 
-    # Проверяем флаг -i
-    if args.info:
-        get_info_all_files(source_dir)
-
-    if args.report:
-        report(source_dir)
+        # Обработка сортировки
+        if args.sorting_meta and args.sorting_name:
+            print("Ошибка: нельзя использовать одновременно сортировку по метаданным и по имени.")
+            sys.exit(1)
+        elif args.sorting_meta:
+            print("Выполняется сортировка файлов по метаданным.")
+            sorting_for_meta(source_dir)
+        elif args.sorting_name:
+            print("Выполняется сортировка файлов по имени.")
+            sorting_for_name(source_dir)
 
 
